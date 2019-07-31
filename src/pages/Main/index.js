c7f6ff0cfb94c0b4faa11c22c15caf770a44bb68
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-07-31 13:21:54 -0300

    Add moment to format date

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,7 @@ import { Container, Form } from './styles';
 import logo from '../../assets/logo.png';
 import CompareList from '../../components/CompareList';
 import api from '../../services/api'
+import moment from 'moment'
 
 
 class Main extends Component{
@@ -16,11 +17,13 @@ class Main extends Component{
     e.preventDefault();
 
     try {
-      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
 
       this.setState({
         repositoryInput:'',
-        repositories: [...this.state.repositories , response.data]
+        repositories: [...this.state.repositories , repository]
       })
     } catch (err) {
       console.log(err)
